4a1bea7e45153d9ca60dbe943886c53fd7d8bef4
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-03 13:07:57 +0000

    [Add] usersの詳細画面と一覧画面作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,11 +20,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
  private
 
