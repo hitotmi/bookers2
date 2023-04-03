ba94a95a7cae0ec1bb0566aaadd186b584640007
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-03 23:34:05 +0000

    [Add] 各画面のレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @booknew = Book.new
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def index
     @user = current_user
     @users = User.all
+    @book = Book.new
   end
   def edit
     @user = User.find(params[:id])
