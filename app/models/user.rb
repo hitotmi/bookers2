b1d10cdfb3ec9cfd8ee1dc4126e3e85a7d076885
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-04 05:10:47 +0000

    [Add] Font Awesomeを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :name, uniqueness: true
+  validates :name, length: { maximum: 50 }
 
   has_one_attached :profile_image
 
