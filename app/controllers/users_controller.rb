b1579f17971f4f2c77cf4323ed8c4e96bb0c933d
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-04 02:35:18 +0000

    [Add] フラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,21 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
   end
+
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+       render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
+
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user)
     else
       render :edit
